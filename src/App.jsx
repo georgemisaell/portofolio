90a9b80b582212cc7a3f01bce6bc826fe652d784
georgemisaell <georgemisaelgantume@gmail.com>
2025-07-25 16:40:56 +0700

    tools section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import DataImage from "./data";
+import { listTools } from "./data";
 
 function App() {
   return (
@@ -81,6 +82,33 @@ function App() {
               </div>
             </div>
           </div>
+        </div>
+      </div>
+
+      {/* Tools */}
+      <div className="tools" mt-32>
+        <h1 className="text-4xl/snug font-bold mb-4">Tools that I often use</h1>
+        <p className="xl:w-2/5 lg:w-2/4 md:w-2/3 sm:w-3/4 w-full text-base/loose opacity-50">
+          Here are some tools that I usually use to create websites.
+        </p>
+
+        <div className="tools-box mt-14 grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
+          {listTools.map((tool) => (
+            <div
+              className="flex items-center gap-2 p-3 border border-zinc-600 rounded-md hover:bg-zinc-800 group"
+              key={tool.id}
+            >
+              <img
+                src={tool.gambar}
+                alt="Tools Image"
+                className="w-14 bg-zinc-800 p-1 group-hover:bg-zinc-900"
+              />
+              <div>
+                <h4 className="font-bold">{tool.nama}</h4>
+                <p className="opacity-50">{tool.ket}</p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </>
