6bccf8ad6c5818e6da526a085dc5d3b025b30c60
georgemisaell <georgemisaelgantume@gmail.com>
2026-03-16 01:35:10 +0700

    feat: integrate 3D astronaut model and update hero section
    - Added @react-three/fiber and @react-three/drei dependencies for 3D rendering.
    - Implemented Astronaut component to display a 3D model in the Hero section.
    - Updated App component to remove ClickSpark and SplashCursor, simplifying the layout.
    - Enhanced Hero section with responsive design using react-responsive.
    - Added new GLB model for the astronaut to the project.

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,24 @@ import About from "./components/section/About";
 import Tools from "./components/section/Tools";
 import Project from "./components/section/Project";
 import Contact from "./components/section/Contact";
-import ClickSpark from "../src/components/reactbits/ClickSpark";
-import SplashCursor from "./components/reactbits/SplashCursor";
 
 function App() {
   return (
     <>
-      <ClickSpark
-        sparkColor="#fff"
-        sparkSize={10}
-        sparkRadius={15}
-        sparkCount={8}
-        duration={400}
-      >
-        {/* Hero Section */}
-        <Hero />
+      {/* Hero Section */}
+      <Hero />
 
-        {/* About Section */}
-        <About />
+      {/* About Section */}
+      <About />
 
-        {/* Tools Section*/}
-        <Tools />
+      {/* Tools Section*/}
+      <Tools />
 
-        {/* Project Section */}
-        <Project />
+      {/* Project Section */}
+      <Project />
 
-        {/* Contact Section */}
-        <Contact />
-        <SplashCursor />
-      </ClickSpark>
+      {/* Contact Section */}
+      <Contact />
     </>
   );
 }
--- src/components/section/Hero.jsx
+++ src/components/section/Hero.jsx
@@ -1,12 +1,31 @@
 import DataImage from "../../data";
 import TypeIt from "typeit-react";
-import ShinyText from "../reactbits/ShinyText";
-import PixelTransition from "../reactbits/PixelTransition";
+import { Astronaut } from "../Astronaut.jsx";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+  const isMobile = useMediaQuery({ maxWidth: 835 });
+
+  /* Canvas */
+  const AstronautCanvas = (
+    <div className="w-full h-[400px] md:h-[600px] relative">
+      <Canvas
+        camera={{ position: [0, 1, 3] }}
+        className={`h-[300px] md:h-[500px] ${isMobile ? "mb-6" : "order-2 md:order-1"}`}
+      >
+        <Astronaut position={[0, -1, -0.5]} />
+        <OrbitControls enableZoom={false} />
+      </Canvas>
+    </div>
+  );
+
   return (
     <div className="hero grid md:grid-cols-2 items-center pt-10 xl:gap-0 gap-6 grid-cols-1">
-      <div className="animate__animated animate__fadeInUp">
+      {!isMobile && <div className="order-2 md:order-1">{AstronautCanvas}</div>}
+      {/* Text */}
+      <div className="animate__animated animate__fadeInUp order-1 md:order-2">
         <div className="flex items-center gap-3 mb-6 bg-zinc-800 w-fit p-4 rounded-2xl">
           <img
             src={DataImage.HeroImage}
@@ -16,6 +35,9 @@ const Hero = () => {
           />
           <q>Beautiful code, born from perseverance</q>
         </div>
+
+        {isMobile && AstronautCanvas}
+
         <h1 className="text-5xl/tight font-bold mb-6">
           <TypeIt>Hi, I'm George Misael</TypeIt>
         </h1>
@@ -42,36 +64,6 @@ const Hero = () => {
           </a>
         </div>
       </div>
-      <PixelTransition
-        firstContent={
-          <img
-            src={DataImage.HeroImage}
-            alt="George Profile Photo"
-            style={{ width: "100%", height: "100%", objectFit: "cover" }}
-            loading="lazy"
-            className="animate__animated animate__fadeInUp"
-          />
-        }
-        secondContent={
-          <div
-            style={{
-              width: "100%",
-              height: "100%",
-              display: "grid",
-              placeItems: "center",
-              backgroundColor: "#18181B",
-            }}
-          >
-            <p style={{ fontWeight: 900, fontSize: "3rem", color: "#ffffff" }}>
-              Nice to meet you!
-            </p>
-          </div>
-        }
-        gridSize={15}
-        pixelColor="#ffffff"
-        animationStepDuration={0.4}
-        className="w-[500px] md:ml-auto"
-      />
     </div>
   );
 };
