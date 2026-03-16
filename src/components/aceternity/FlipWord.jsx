2ac151a5ed644a082e1dc6450feeb3ea750b21f8
georgemisaell <georgemisaelgantume@gmail.com>
2026-03-16 10:10:49 +0700

    fix: adjust font size in FlipWord component

--- src/components/aceternity/FlipWord.jsx
+++ src/components/aceternity/FlipWord.jsx
@@ -5,7 +5,7 @@ export function FlipWord() {
   const words = ["Developer", "Engineer"];
 
   return (
-    <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
+    <div className="text-3xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
       George Misael <br />
       Software <FlipWords words={words} />
     </div>
