669148f7a6d0a0fb23e8b6d5b674928c53811498
georgemisaell <georgemisaelgantume@gmail.com>
2026-03-16 12:47:56 +0700

    feat: add tools display

---
+++ src/components/magicui/Marquee.jsx
@@ -0,0 +1,111 @@
+import { cn } from "../../../lib/utils";
+import { Marquee } from "../ui/Marquee";
+
+const reviews = [
+  {
+    name: "Github",
+    img: "./public/assets/tools/github.png",
+  },
+  {
+    name: "Figma",
+    img: "./public/assets/tools/figma.png",
+  },
+  {
+    name: "Golang",
+    img: "./public/assets/tools/golang.png",
+  },
+  {
+    name: "NodeJS",
+    img: "./public/assets/tools/nodejs.png",
+  },
+  {
+    name: "NextJS",
+    img: "./public/assets/tools/nextjs.png",
+  },
+  {
+    name: "Tailwind",
+    img: "./public/assets/tools/tailwind.png",
+  },
+  {
+    name: "ReactJS",
+    img: "./public/assets/tools/reactjs.png",
+  },
+  {
+    name: "Wordpress",
+    img: "./public/assets/tools/wordpress.png",
+  },
+  {
+    name: "Postman",
+    img: "./public/assets/tools/postman.png",
+  },
+  {
+    name: "Javascript",
+    img: "./public/assets/tools/js.png",
+  },
+  {
+    name: "Python",
+    img: "./public/assets/tools/python.png",
+  },
+  {
+    name: "PostgreSQL",
+    img: "./public/assets/tools/postgresql.png",
+  },
+  {
+    name: "Git",
+    img: "./public/assets/tools/git.png",
+  },
+  {
+    name: "CSS",
+    img: "./public/assets/tools/css.png",
+  },
+];
+
+const shuffledReviews = [...reviews].sort(() => Math.random() - 0.5);
+
+const chunkSize = Math.ceil(shuffledReviews.length / 3);
+
+const firstRow = shuffledReviews.slice(0, chunkSize);
+const secondRow = shuffledReviews.slice(chunkSize, chunkSize * 2);
+const thirdRow = shuffledReviews.slice((chunkSize * 2) / 3);
+
+const ReviewCard = ({ img, name, body }) => {
+  return (
+    <figure
+      className={cn("relative  cursor-pointer overflow-hidden rounded-xl  p-4")}
+    >
+      <div className="flex flex-col items-center">
+        <img width="50" alt="" src={img} />
+        <div className="flex flex-col mt-4">
+          <figcaption className="text-sm font-medium dark:text-white">
+            {name}
+          </figcaption>
+        </div>
+      </div>
+      <blockquote className="mt-2 text-sm">{body}</blockquote>
+    </figure>
+  );
+};
+
+export function MarqueeDemo() {
+  return (
+    <div className="relative flex w-full flex-col items-center justify-center overflow-hidden">
+      <Marquee pauseOnHover className="[--duration:20s]">
+        {firstRow.map((review) => (
+          <ReviewCard key={review.name} {...review} />
+        ))}
+      </Marquee>
+      <Marquee reverse pauseOnHover className="[--duration:20s]">
+        {secondRow.map((review) => (
+          <ReviewCard key={review.name} {...review} />
+        ))}
+      </Marquee>
+      <Marquee pauseOnHover className="[--duration:20s]">
+        {thirdRow.map((review) => (
+          <ReviewCard key={review.name} {...review} />
+        ))}
+      </Marquee>
+      <div className="from-background pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-gradient-to-r"></div>
+      <div className="from-background pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-gradient-to-l"></div>
+    </div>
+  );
+}
--- src/components/section/Tools.jsx
+++ src/components/section/Tools.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { listTools } from "../../data";
+import { MarqueeDemo } from "../magicui/Marquee";
 const Tools = () => {
   return (
     <div className="tools mt-32">
@@ -10,7 +10,7 @@ const Tools = () => {
         data-aos-delay="100"
         data-aos-once="true"
       >
-        Tools that I often use
+        Tools and tech stacks that I used
       </h1>
       <p
         className="xl:w-2/5 lg:w-2/4 md:w-2/3 sm:w-3/4 w-full text-base/loose opacity-50"
@@ -22,27 +22,8 @@ const Tools = () => {
         Here are some tools that I usually use to create websites.
       </p>
 
-      <div className="tools-box mt-14 grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
-        {listTools.map((tool) => (
-          <div
-            className="flex items-center gap-2 p-3 border border-zinc-600 rounded-md hover:bg-zinc-800 group"
-            key={tool.id}
-            data-aos="fade-up"
-            data-aos-duration="1000"
-            data-aos-delay={tool.dad}
-            data-aos-once="true"
-          >
-            <img
-              src={tool.gambar}
-              alt="Tools Image"
-              className="w-14 bg-zinc-800 p-1 group-hover:bg-zinc-900"
-            />
-            <div>
-              <h4 className="font-bold">{tool.nama}</h4>
-              <p className="opacity-50">{tool.ket}</p>
-            </div>
-          </div>
-        ))}
+      <div className="mt-14">
+        <MarqueeDemo />
       </div>
     </div>
   );
