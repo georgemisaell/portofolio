0080184623859ca6794befba8f2b33131a2d5a79
georgemisaell <georgemisaelgantume@gmail.com>
2026-03-17 22:50:54 +0700

    fix: remove unnecessary line breaks in motivational quote in Hero component

--- src/components/section/Hero.jsx
+++ src/components/section/Hero.jsx
@@ -43,8 +43,7 @@ const Hero = () => {
             loading="lazy"
           />
           <p>
-            Shoot for the <b>moon</b>. Even if you miss, you’ll land among the{" "}
-            <b>stars</b>.
+            Shoot for the <b>moon</b>. Even if you miss, you’ll land among the <b>stars</b>.
           </p>
         </div>
 
